90f6fc2a2b5404d0300c541d70723bd2a23e9b71
Ben <benpaullamb@gmail.com>
2021-02-21 16:59:02 +0000

    feat: config now in cm, scaling to aspect ratio, and html vis

--- README.md
+++ README.md
@@ -9,3 +9,10 @@
 5. Adjust the config in `config.json`
 6. `npm run slice`
 7. View your sliced image in the "output" folder
+
+## TODO
+
+- Add canvas and spacing size to config in cm
+- Calculate the total ratio of the canvas with spaces
+- Scale the image to that ratio before slicing
+- Calculate the spacing in px from the config
--- slice.js
+++ slice.js
@@ -3,8 +3,7 @@ const path = require('path');
 const Jimp = require('jimp');
 const config = require('./config.json');
 
-const { sliceCount, sliceGap } = config;
-const halfSliceGap = sliceGap / 2;
+const { panelCount } = config;
 
 (async () => {
 	console.log('Starting Image Slicer');
@@ -22,30 +21,31 @@ const halfSliceGap = sliceGap / 2;
 	console.log(`Slicing: ${imageNameWithExt}`);
 
 	try {
-		const image = await Jimp.read(path.join('./input', imageNameWithExt));
-		image.quality(100);
-		const sliceWidth = image.bitmap.width / sliceCount;
-		const height = image.bitmap.height;
+		let image = await Jimp.read(path.join('./input', imageNameWithExt));
 
-		for (let i = 0; i < sliceCount; i++) {
+		console.log(`Original size: ${image.bitmap.width}x${image.bitmap.height}`);
+
+		image.quality(100);
+		image = scaleToAspectRatio(image);
+
+		console.log(`Scaled size: ${image.bitmap.width}x${image.bitmap.height}`);
+
+		const sliceWidth = image.bitmap.width / panelCount;
+		const height = image.bitmap.height;
+		const gap = pixelsGap(image);
+		const halfGap = gap / 2;
+
+		console.log(`A ${config.panelGap}cm gap is ${Math.round(gap)}px`);
+
+		for (let i = 0; i < panelCount; i++) {
 			const slice = image.clone();
 
 			if (i === 0) {
-				slice.crop(i * sliceWidth, 0, sliceWidth - halfSliceGap, height);
-			} else if (i === sliceCount - 1) {
-				slice.crop(
-					i * sliceWidth + halfSliceGap,
-					0,
-					sliceWidth - halfSliceGap,
-					height
-				);
+				slice.crop(0, 0, sliceWidth - halfGap, height);
+			} else if (i === panelCount - 1) {
+				slice.crop(i * sliceWidth + halfGap, 0, sliceWidth - halfGap, height);
 			} else {
-				slice.crop(
-					i * sliceWidth + halfSliceGap,
-					0,
-					sliceWidth - sliceGap,
-					height
-				);
+				slice.crop(i * sliceWidth + halfGap, 0, sliceWidth - gap, height);
 			}
 
 			const sliceName = `${imageName}-${i + 1}.${image.getExtension()}`;
@@ -61,3 +61,30 @@ const halfSliceGap = sliceGap / 2;
 
 	console.log('Slicing complete.');
 })();
+
+const totalWidthCm = () => {
+	return (
+		config.panelWidth * config.panelCount +
+		config.panelGap * (config.panelCount - 1)
+	);
+};
+
+const scaleToAspectRatio = (image) => {
+	console.log('Resizing to canvas aspect ratio');
+
+	const scaledImage = image.clone();
+	// 1 height : x width
+	const heightToWidthRatio = totalWidthCm() / config.panelHeight;
+
+	const newWidth = heightToWidthRatio * scaledImage.bitmap.height;
+
+	scaledImage.cover(newWidth, scaledImage.bitmap.height);
+
+	return scaledImage;
+};
+
+const pixelsGap = (image) => {
+	const pxPerCm = image.bitmap.width / totalWidthCm();
+
+	return config.panelGap * pxPerCm;
+};
